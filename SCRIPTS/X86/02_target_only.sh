d88b4dc1c294e5c4af12976c2a26f4c21807a126
xujian1971 <150315383+xujian1971@users.noreply.github.com>
2024-04-03 19:31:57 +0800

    Update 02_target_only.sh

--- SCRIPTS/X86/02_target_only.sh
+++ SCRIPTS/X86/02_target_only.sh
@@ -30,6 +30,9 @@ wget https://downloads.openwrt.org/releases/${latest_version}/targets/x86/64/pac
 zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' >.vermagic
 sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
 
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
 # 预配置一些插件
 cp -rf ../PATCH/files ./files
 
