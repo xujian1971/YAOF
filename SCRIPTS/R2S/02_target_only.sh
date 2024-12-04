44f9af6ef5924def564ead0bcf366e7cda6193c0
QiuSimons <45143996+QiuSimons@users.noreply.github.com>
2024-12-04 20:57:02 +0800

    fix kmod src

--- SCRIPTS/R2S/02_target_only.sh
+++ SCRIPTS/R2S/02_target_only.sh
@@ -8,11 +8,19 @@ sed -i 's,-mcpu=generic,-march=armv8-a,g' include/target.mk
 sed -i 's,"eth1" "eth0","eth0" "eth1",g' target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
-#Vermagic
+# Vermagic
+target="rockchip/armv8"
 latest_version="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][4-9]/p' | sed -n 1p | sed 's/v//g' | sed 's/.tar.gz//g')"
-wget https://downloads.openwrt.org/releases/${latest_version}/targets/rockchip/armv8/profiles.json
+wget -O profiles.json https://downloads.openwrt.org/releases/${latest_version}/targets/${target}/profiles.json
+
 jq -r '.linux_kernel.vermagic' profiles.json | tr -d '\n' >.vermagic
 sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
+
+# kmod feed hack
+wget -O sha256sums https://downloads.openwrt.org/releases/${latest_version}/targets/${target}/sha256sums
+kmods_path=$(awk '$2 ~ /^\*kmods/ { match($2, /kmods[^ ]+/, arr); split(arr[0], path_parts, "/"); print path_parts[1] "/" path_parts[2]; exit }' "./sha256sums")
+mkdir -p ./files/etc/opkg
+echo "src/gz openwrt_kmods https://mirrors.pku.edu.cn/openwrt/releases/${latest_version}/targets/${target}/$kmods_path" > ./files/etc/opkg/customfeeds.conf
 
 # 预配置一些插件
 cp -rf ../PATCH/files ./files
